2a391012f8914f703f691f28c59f7910ef4d8fbd
root <akiraneko@gmail.com>
2024-05-14 18:22:01 +0900

    update

---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+git pull
+
+# 
+cd datasheet_en
+# https://www.wch-ic.com/downloads/CH32V003DS0_PDF.html
+#wget --continue https://www.wch-ic.com/downloads/file/359.html -O CH32V003DS0.PDF
+# https://www.wch-ic.com/downloads/CH32V003RM_PDF.html
+#wget --continue https://www.wch-ic.com/downloads/file/358.html -O CH32V003RM.PDF
+cd ..
+
+# 
+cd datasheet_zh
+# https://www.wch.cn/downloads/CH32V002DS0_PDF.html
+wget --continue https://www.wch-ic.com/downloads/file/473.html -O CH32V002DS0.PDF
+# https://www.wch.cn/downloads/CH32V004DS0_PDF.html
+wget --continue https://www.wch-ic.com/downloads/file/474.html -O CH32V004DS0.PDF
+# https://www.wch.cn/downloads/CH32V006DS0_PDF.html
+wget --continue https://www.wch-ic.com/downloads/file/475.html -O CH32V006DS0.PDF
+# https://www.wch.cn/downloads/CH32V002DS0_PDF.html
+wget --continue https://www.wch-ic.com/downloads/file/473.html -O CH32V002DS0.PDF
+# https://www.wch.cn/downloads/CH32V00XRM_PDF.html
+wget --continue https://www.wch-ic.com/downloads/file/472.html -O CH32V00XRM.PDF
+cd ..
+
+# https://www.wch.cn/downloads/CH32V006EVT_ZIP.html
+wget --continue https://www.wch.cn/downloads/file/477.html -O CH32V006EVT.ZIP
+rm -rfv EVT
+unzip *.ZIP
+
+git add . --all
+git commit -m "update"
+git push origin main
