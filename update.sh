6c1bcc5234966ec72dfa92ba5c0bf453f1eac352
root <akiraneko@gmail.com>
2024-05-15 10:45:44 +0900

    update

--- update.sh
+++ update.sh
@@ -29,7 +29,7 @@ cd ..
 # https://www.wch.cn/downloads/CH32V006EVT_ZIP.html
 wget --continue https://www.wch.cn/downloads/file/477.html -O CH32V006EVT.ZIP
 rm -rfv EVT
-unzip *.ZIP
+unzip -O utf-8 *.ZIP
 
 git add . --all
 git commit -m "update"
