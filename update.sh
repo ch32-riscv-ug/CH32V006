436c5091ebf105e372c7dfa115241bd845d53e28
root <tanakamasayuki@gmail.com>
2025-04-14 15:59:36 +0900

    update

--- update.sh
+++ update.sh
@@ -21,19 +21,19 @@ cd ..
 # 
 cd datasheet_zh
 # https://www.wch.cn/downloads/CH32V002DS0_PDF.html
-curl -z CH32V002DS0.PDF -o CH32V002DS0.PDF https://www.wch.cn/downloads/file/473.html
+curl -z CH32V002DS0.PDF -o CH32V002DS0.PDF https://www.wch.cn/download/file?id=473
 # https://www.wch.cn/downloads/CH32V004DS0_PDF.html
-curl -z CH32V004DS0.PDF -o CH32V004DS0.PDF https://www.wch.cn/downloads/file/474.html
+curl -z CH32V004DS0.PDF -o CH32V004DS0.PDF https://www.wch.cn/download/file?id=474
 # https://www.wch.cn/downloads/CH32V006DS0_PDF.html
-curl -z CH32V006DS0.PDF -o CH32V006DS0.PDF https://www.wch.cn/downloads/file/475.html
+curl -z CH32V006DS0.PDF -o CH32V006DS0.PDF https://www.wch.cn/download/file?id=475
 # https://www.wch.cn/downloads/CH32V00XRM_PDF.html
-curl -z CH32V00XRM.PDF -o CH32V00XRM.PDF https://www.wch.cn/downloads/file/472.html
+curl -z CH32V00XRM.PDF -o CH32V00XRM.PDF https://www.wch.cn/download/file?id=472
 # https://www.wch.cn/downloads/CH32V007DS0_PDF.html
-curl -z CH32V007DS0.PDF -o CH32V007DS0.PDF https://www.wch.cn/downloads/file/476.html
+curl -z CH32V007DS0.PDF -o CH32V007DS0.PDF https://www.wch.cn/download/file?id=476
 cd ..
 
 # https://www.wch.cn/downloads/CH32V006EVT_ZIP.html
-curl -z CH32V006EVT.ZIP -o CH32V006EVT.ZIP https://www.wch.cn/downloads/file/477.html
+curl -z CH32V006EVT.ZIP -o CH32V006EVT.ZIP https://www.wch.cn/download/file?id=477
 rm -rfv EVT
 unzip -O GB2312 *.ZIP
 
